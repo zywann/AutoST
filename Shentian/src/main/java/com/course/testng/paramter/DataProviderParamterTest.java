5f04a44616e805bea4914d2e09c2cb42d18b1b36
zywann <348441707@qq.com>
2020-07-05 19:01:17 +0800

    testng DataProvider-对不同方法进行参数化

--- Shentian/src/main/java/com/course/testng/paramter/DataProviderParamterTest.java
+++ Shentian/src/main/java/com/course/testng/paramter/DataProviderParamterTest.java
@@ -3,8 +3,14 @@ package com.course.testng.paramter;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class DataProviderParamterTest {
+import java.lang.reflect.Method;
 
+public class DataProviderParamterTest {
+    /**
+     * 普通的DataProvider参数化传值
+     * @param name
+     * @param age
+     */
     @Test(dataProvider = "data")
     public void testMethod(String name,int age){
         System.out.println("name = "+name+";  age = "+age);
@@ -20,4 +26,33 @@ public class DataProviderParamterTest {
         return o;
     }
 
+    /**
+     * 根据方法不同，进行不同的参数化传递
+     * @param name
+     * @param age
+     */
+    @Test(dataProvider = "methodData")
+    public void test1(String name,int age){
+        System.out.println("这是test111方法：  name = "+name+";  age = "+age);
+    }
+    @Test(dataProvider = "methodData")
+    public void test2(String name,int age){
+        System.out.println("这是test222方法：  name = "+name+";  age = "+age);
+    }
+    @DataProvider(name = "methodData")
+    public Object[][] dataProviderMethod1(Method method){ //获取method类对象
+        Object[][] result = null;
+        if (method.getName().equals("test1")){ //获取需要传值的方法
+            result = new Object[][]{
+                    {"zhangsan",20},
+                    {"lisi",21}
+            };
+        }else if (method.getName().equals("test2")){
+            result = new Object[][]{
+                    {"wangwu",26},
+                    {"zhaoliu",27}
+            };
+        }
+        return result;
+    }
 }
