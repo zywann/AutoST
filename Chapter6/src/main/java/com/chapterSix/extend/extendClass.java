af6fd2cede814a9962b81a2a5d1117f277782b66
zywann <348441707@qq.com>
2020-07-06 18:45:24 +0800

    extendReport 测试报告

---
+++ Chapter6/src/main/java/com/chapterSix/extend/extendClass.java
@@ -0,0 +1,28 @@
+package com.chapterSix.extend;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class extendClass {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,2);
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void testLog(){
+        Reporter.log("这是我们自己写日志");
+        throw new RuntimeException("自定义运行时异常");
+    }
+
+}
