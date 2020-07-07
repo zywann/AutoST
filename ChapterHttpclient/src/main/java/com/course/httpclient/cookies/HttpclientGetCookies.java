3e4cbdbd5d12a540eb734f12fe7e2a230df13e31
zywann <348441707@qq.com>
2020-07-07 23:38:25 +0800

    获取cookies

--- ChapterHttpclient/src/main/java/com/course/httpclient/cookies/HttpclientGetCookies.java
+++ ChapterHttpclient/src/main/java/com/course/httpclient/cookies/HttpclientGetCookies.java
@@ -1,14 +1,17 @@
 package com.course.httpclient.cookies;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HttpclientGetCookies {
@@ -42,5 +45,16 @@ public class HttpclientGetCookies {
         // 获取响应信息
         result = EntityUtils.toString(response.getEntity());
         System.out.println(result);
+
+
+        // 获取 cookies
+        //
+        CookieStore store =((DefaultHttpClient) client).getCookieStore();
+        List<Cookie> cookies = store.getCookies();
+        for (Cookie cookie: cookies){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookies name = "+name+" cookies value = "+value);
+        }
     }
 }
